9e2ac279e7b54d2f095de13d9b8aa140ffe6a347
nipoko <hachi@8-degrees.info>
2015-06-22 13:58:10 +0900

    volume0-0019を追加

---
+++ volume0/0019/factorial.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+=begin
+AOJ問題 volume0-0019 階乗
+整数nを入力し、nの階乗を出力して終了するプログラムを作成してください。
+ただし、nは1以上20以下とします。
+[input]
+n（整数）
+[output]
+nの階乗
+[sample input]
+5
+[sample output]
+120
+=end
+
+class Fixnum
+  def factorial
+    case self
+    when 1 then 1
+    else self * (self - 1).factorial
+    end
+  end
+end
+
+puts File.read("./input.txt").to_i.factorial
