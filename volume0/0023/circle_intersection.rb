704523daab7c9a5754f2decfbe6e799f829ef89f
nipoko <hachi@8-degrees.info>
2015-06-25 16:33:01 +0900

    volume0-0023の処理を追加

--- volume0/0023/circle_intersection.rb
+++ volume0/0023/circle_intersection.rb
@@ -20,3 +20,43 @@ xa ya ra xb yb rb
 2
 0
 =end
+
+# 円の中心間のベクトル : v→ = (x2-x1,y2-y1)
+# vの長さ：d = |v| = sqrt((x2-x1)^2+(y2-y1)^2)
+#
+# 円の位置関係
+#
+# d＞r1+r2のとき、交点はない。（離れている）
+# d＝r1+r2のとき、交点は1つ。（外接）
+# |r1-r2|＜d＜r1+r2のとき、交点は2つ。（交わる）
+# |r1-r2|＝dのとき、交点は1つ。（内接）
+# |r1-r2|＞dのとき、交点はない。（内包）
+# r1=r2かつd=0のとき、交点は無数にある。（一致）
+
+class Array
+  def length
+    Math.sqrt((self[3] - self[0]) ** 2 + (self[4] - self[1]) ** 2)
+  end
+
+  def circle_intersection
+    length = self.length
+    if length > self[2] + self[5] then
+      return 0
+    elsif (self[2] - self[5]).abs <= length && length <= self[2] + self[5] then
+      return 1
+    elsif self[2] > self[5] then
+      return 2
+    elsif self[2] < self[5] then
+      return -2
+    else
+      return -9999
+    end
+  end
+end
+
+file = File.readlines("./input.txt").map(&:chomp)
+file.shift
+
+file.each do |data|
+  puts data.split(" ").map(&:to_f).circle_intersection
+end
